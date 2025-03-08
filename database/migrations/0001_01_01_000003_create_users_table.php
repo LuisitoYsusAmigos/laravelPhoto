4ad23b452363c80baecfebddbec390ee6c5cf259
Luis <lfgb2001@hotmail.com>
2025-03-08 04:47:37 -0400

    correcion migraciones

--- database/migrations/0001_01_01_000003_create_users_table.php
+++ database/migrations/0001_01_01_000003_create_users_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('id_sucursal')->constrained('sucursal')->onDelete('cascade'); 
+            $table->foreignId('id_sucursal')->constrained('sucursal')->onDelete('cascade');
+
             $table->rememberToken();
             $table->timestamps();
         });
